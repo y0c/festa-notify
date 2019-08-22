4960429a93a395a169018ae46aead2125462ce23
y0c <holnet1026@gmail.com>
2019-08-22 09:05:13 +0000

    refactor: change mime type to text/html

--- mail/mail.go
+++ mail/mail.go
@@ -31,7 +31,7 @@ func New(to []string, subject string, body string) *Request {
 }
 
 func (r *Request) Send() (bool, error) {
-	mime := "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 	subject := "Subject: " + r.subject + "!\n"
 	msg := []byte(subject + mime + "\n" + r.body)
 	addr := "smtp.gmail.com:587"
