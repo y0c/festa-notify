ed16aada922e7ec328bb43ff5206050cc9657b88
y0c <holnet1026@gmail.com>
2019-08-22 09:05:49 +0000

    feat: implement generate event template

---
+++ template/event.go
@@ -0,0 +1,67 @@
+package template
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/y0c/festa-notify/festa"
+	"html/template"
+)
+
+type TemplateVariables struct {
+	Events []festa.Event
+}
+
+const mailTemplate = `
+	<html>
+		<head>
+			<meta charset="utf-8" />
+		</head>
+		<body>
+			<ul>
+			 {{range .Events}}
+					<li>
+						<h3><a href="https://festa.io/events/{{ .EventID }}">{{ .Name }}</a></h3>
+						<ul>
+							{{range .Tickets}}
+								<li>
+									<strong>{{ .Name }}</strong>
+									<div>
+										<span>마감여부 - </span><span>{{ if .Registable }} 등록가능 {{ else }} 마감 {{ end }}<span>
+									</div>
+									<div>
+										<span>가격 - </span><span>{{ .Price }}원<span>
+									</div>
+									<div>
+										<span>{{ .LimitPerUser }} 개 남음</span>
+									</div>
+								</li>
+							{{end}}
+						</ul>
+					</li>		
+			 {{end}}
+			</ul>
+		</body>
+	</html>
+`
+
+func GenerateEventTemplate(events []festa.Event) (string, error) {
+
+	var buf bytes.Buffer
+	t := template.New("MailTemplate")
+
+	tmpl, err := t.Parse(mailTemplate)
+
+	if err != nil {
+		return "", fmt.Errorf("template error %v", err)
+	}
+
+	err = tmpl.Execute(&buf, TemplateVariables{
+		events,
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("template error %v", err)
+	}
+
+	return buf.String(), nil
+}
