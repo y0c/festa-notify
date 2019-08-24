1df3b48f7a018e988ee906b14d0ce55c824b1a9c
Hosung Lim <holnet1026@gmail.com>
2019-08-24 23:54:16 +0900

    refactor: change gmail to aws ses

--- mail/mail.go
+++ mail/mail.go
@@ -1,43 +1,61 @@
 package mail
 
 import (
-	"net/smtp"
-	"os"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
 )
 
-type Request struct {
-	from    string
-	to      []string
-	subject string
-	body    string
+type EmailData struct {
+	From    string
+	To      string
+	Subject string
+	Body    string
 }
 
-var auth smtp.Auth
-var mailId string
-var mailPw string
+const Sender = "noreply@festa-notify.cf"
+const CharSet = "UTF-8"
 
-func Auth() {
-	mailId = os.Getenv("GMAIL_ID")
-	mailPw = os.Getenv("GMAIL_PW")
-	auth = smtp.PlainAuth("", mailId, mailPw, "smtp.gmail.com")
-}
+func Send(data EmailData) error {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
 
-func New(to []string, subject string, body string) *Request {
-	return &Request{
-		to:      to,
-		subject: subject,
-		body:    body,
+	if err != nil {
+		return err
 	}
-}
 
-func (r *Request) Send() (bool, error) {
-	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
-	subject := "Subject: " + r.subject + "!\n"
-	msg := []byte(subject + mime + "\n" + r.body)
-	addr := "smtp.gmail.com:587"
+	svc := ses.New(sess)
 
-	if err := smtp.SendMail(addr, auth, mailId, r.to, msg); err != nil {
-		return false, err
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(data.To),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(data.Body),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(CharSet),
+				Data:    aws.String(data.Subject),
+			},
+		},
+		Source: aws.String(Sender),
 	}
-	return true, nil
+
+	result, err := svc.SendEmail(input)
+
+	fmt.Println(result)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
